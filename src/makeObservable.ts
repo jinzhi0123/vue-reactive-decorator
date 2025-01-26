06ff254dd8b97e8d5cc8d1f2e5bad8cadfdb238c
jinzhi0123 <grinawkward@gmail.com>
2025-01-27 00:56:42 +0800

    fix: ensure correct property processing order in makeObservable

--- src/makeObservable.ts
+++ src/makeObservable.ts
@@ -1,16 +1,49 @@
+import type { ReactiveDecorator } from './decorator'
 import { asObservableObject, STORED_ADM_KEY } from './adm'
+import { isComputed } from './computed'
+import { isObservable } from './observable'
 import { collectDecorators } from './store'
 import { ownKeys } from './utils'
+import { isWatch } from './watch'
+import { isWatchEffect } from './watchEffect'
+
+type DecoratorType = 'observable' | 'computed' | 'watch'
+
+interface DecoratorRecord {
+  key: PropertyKey
+  decorator: ReactiveDecorator
+  type: DecoratorType
+}
+
+function getDecoratorType(decorator: ReactiveDecorator): DecoratorType | null {
+  if (isObservable(decorator))
+    return 'observable'
+  if (isComputed(decorator))
+    return 'computed'
+  if (isWatch(decorator) || isWatchEffect(decorator))
+    return 'watch'
+  return null
+}
 
 export function makeObservable<T extends object>(target: T): T {
   const decorators = collectDecorators(target)
-
   const adm = asObservableObject(target)[STORED_ADM_KEY]
 
-  ownKeys(decorators).forEach((key) => {
-    const decorator = decorators[key]
-    // const descriptor = getDescriptor(target, key)
+  const decoratorRecords = ownKeys(decorators)
+    .map(key => ({
+      key,
+      decorator: decorators[key],
+      type: getDecoratorType(decorators[key]),
+    }))
+    .filter((record): record is DecoratorRecord => record.type !== null)
+    .sort((a, b) => {
+      const order = { observable: 0, computed: 1, watch: 2 }
+      return order[a.type] - order[b.type]
+    })
+
+  decoratorRecords.forEach(({ key, decorator }) => {
     adm.make(key, decorator)
   })
+
   return target
 }
