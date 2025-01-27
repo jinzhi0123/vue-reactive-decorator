4564a8946dbd7c420f700b33fbe5699ac7ed0a44
jinzhi0123 <grinawkward@gmail.com>
2025-01-27 14:32:29 +0800

    chore: use different tsconfigs for lib and node

---
+++ global.d.ts
@@ -0,0 +1,6 @@
+export {}
+
+declare global {
+  // eslint-disable-next-line no-var, vars-on-top
+  var __DEV__: boolean
+}
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,7 @@
 globalThis.__DEV__ = true
+
+// runtime polyfill
+if (typeof Symbol.metadata === 'undefined') {
+  // @ts-expect-error runtime polyfill
+  Symbol.metadata = Symbol('Symbol.metadata')
+}
